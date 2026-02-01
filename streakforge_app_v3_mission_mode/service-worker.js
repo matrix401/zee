5423b055a042181d4ebef3867bafaefe680b06eb
matrix401 <93835276+matrix401@users.noreply.github.com>
2026-02-01 21:04:30 +0530

    Add files via upload

---
+++ streakforge_app_v3_mission_mode/service-worker.js
@@ -0,0 +1,9 @@
+const CACHE="streakforge-cache-v3";
+const ASSETS=["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):Promise.resolve()))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
+  }).catch(()=>caches.match("./index.html"))));
+});
