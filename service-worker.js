90a6b7555e1068da959bce397c3096391c2d6c9a
matrix401 <93835276+matrix401@users.noreply.github.com>
2026-02-01 21:16:34 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="streakforge-cache-v1";
+const CACHE="streakforge-cache-v3";
 const ASSETS=["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):Promise.resolve()))).then(()=>self.clients.claim()))});
